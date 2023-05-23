2a920419ce75c5ffa97ec6e5b429432ab0b01557
William-P1co <ferneypikin@gmail.com>
2023-05-23 15:59:12 -0500

    first

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="css/css/bootstrap.min.css">
+  <script src=""></script>
+  <title>Document</title>
+</head>
+<body>
+ <h1>FORMULARIO DE REGISTRO DE FACTURACION</h1>
+  <div class="container rounded-3 border border-primary" >
+      <div class="frm"   class="">
+          <div class="row">
+              <div class="col-6">
+                  <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Factura</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="ingresa tu nombre">
+                  </div>
+              </div>
+              <div class="col-6">
+                  <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Fecha</label>
+                    <input type="date" class="form-control" id="exampleFormControlInput1">
+                  </div>
+              </div>
+          </div>
+                <div class="row">
+                  <div class="col-6">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="ingresa tu nombre">
+                    </div>
+                  </div>
+                  <div class="col-6">
+                      <div class="mb-5">
+                          <label for="exampleFormControlInput1" class="form-label">Cedula</label>
+                          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="ingresa tu documento">
+                      </div>
+                  </div>
+                </div>
+                </div>
+                <div class="container mt-4 text-center" >
+                  <input type="submit" class="btn btn-primary" value="Guardar Clientes">
+                  <input type="reset"  class="btn btn-primary" value="Borrar">
+            </div>
+        </div>
+  </div>
+</body>
+</html>
