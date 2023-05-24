e565d4e606bcba5d0f6fa835e40d7b31c23dc03c
William-P1co <ferneypikin@gmail.com>
2023-05-24 09:14:50 -0500

    botones de + and -

--- index.php
+++ index.php
@@ -54,21 +54,27 @@
 
     <div class="container border border-primary p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end">
       <div class="row">
-        <div class="col-3">
+        <div class="col-2">
            <label for="exampleFormControlInput1" class="form-label">Nombre</label>
            <input type="text" class="form-control" id="nombre" placeholder="">
         </div>
-            <div class="col-3">
+            <div class="col-2">
               <label for="exampleFormControlInput1" class="form-label">valor</label>
               <input type="text" class="form-control" id="valor" placeholder="">
             </div>
-            <div class="col-3">
+            <div class="col-2">
               <label for="exampleFormControlInput1" class="form-label">Cantidad</label>
               <input type="text" class="form-control" id="cantidad" placeholder="">
             </div>
-            <div class="col-3">
+            <div class="col-2">
               <label for="exampleFormControlInput1" class="form-label">total</label>
               <input type="text" class="form-control" id="total" placeholder="">
+            </div>
+            <div class="col-2"><br>
+            <button type="button" class="btn btn-outline-success">+</button>
+            </div>
+            <div class="col-2"><br>
+            <button type="button" class="btn btn-outline-danger">-</button>
             </div>
       </div>
     </div>
